c761258617283c8d1a0af9c81ece093c7ac02d80
vitorianuness01 <vitoria.nunes01@fatec.sp.gov.br>
2024-11-25 17:18:08 -0300

    envio de commit

--- frontend/src/app/cadastro/cadastro.component.ts
+++ frontend/src/app/cadastro/cadastro.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
-  standalone: true,
-  imports: [],
+  standalone: true,  // Usado se o componente for standalone, se for parte de um módulo, remova isso
+  imports: [FormsModule],  // Adicionar qualquer módulo que o componente precise aqui, como FormsModule, etc.
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.css'
+  styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent {
+  nomeCompleto: string = ''; 
+  nomeSocial: string = '';     
+  nomeDaMae: string = '';      
+  dataNascimento: string = ''; 
+  sexo: string = '';           
+  nacionalidade: string = '';  
+  municipioNascimento: string = ''; 
+  racaCor: string = '';        
+  frequentaEscola: boolean = false; 
+  escolaridade: string = '';   
+  situacaoFamiliar: string = ''; 
+  estabelecimentoVinculo: string = ''; 
+  estabelecimentoCadastro: string = ''; 
+  deficiencia: string = '';    
+  deficienciaVisual: boolean = false; 
+  deficienciaAuditiva: boolean = false; 
+  deficienciaMotora: boolean = false; 
+  deficienciaIntelectual: boolean = false; 
+  dadosContatoCelular: string = '';  
+  dadosContatoResidencial: string = ''; 
+  dadosContatoComercial: string = ''; 
+  dadosContatoContato: string = ''; 
+  dadosContatoEmail: string = ''; 
+  origemEndereco: string = '';   
+  dadosEndereco: string = '';    
+  cpf: string = '';              
+  identidade: string = '';       
 
+  // Método para submissão do formulário
+  onSubmit(): void {
+    // Aqui você pode realizar o que for necessário com os dados do formulário
+    console.log("Formulário enviado:", this);
+  }
 }
